0489013627c50ed0b5ee35cfe35e4e1798feef2d
guojunxu <gjxu@kaikeba.com>
2015-08-08 13:37:29 +0800

    1 add app description

--- JJTree/JJTAboutViewController.m
+++ JJTree/JJTAboutViewController.m
@@ -9,6 +9,8 @@
 #import "JJTAboutViewController.h"
 #import "JJTRoleTableViewCell.h"
 
+static NSString *ABOUT_JJTREE = @"元机经是一个为程序员们记录和分享编程开发知识点和解决方案的平台。\n\n它不是问答社区，也不是发表文章的平台，它专注于记录某个具体而客观的知识点或解决方案。\n\n如果你愿意，我们鼓励你将个人编写的机经分享至互联网，以为其他网友提供方便。与此同时，你也可能收获网友们打赏的金钱或其他回报。";
+
 @interface JJTAboutViewController ()<UITableViewDataSource, UITableViewDelegate>
 
 @property (nonatomic, weak) IBOutlet UITableView *aboutTableView;
@@ -93,6 +95,10 @@
     }
     
     return 88.f * 3.2;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    return ABOUT_JJTREE;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section{
